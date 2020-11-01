8e59f7142f6d2e0ae24ba8fed6851b66d46324a6
Lakshya2610 <lakshyagupta26@live.com>
2020-11-01 01:36:46 -0400

    fixed errors causing build fail

--- app/websona/lib/scan.dart
+++ app/websona/lib/scan.dart
@@ -51,10 +51,10 @@ class ScanState extends State<ScanScreen> {
 
   Future scan() async {
     try {
-      String barcode = await BarcodeScanner.scan();
+      String barcode = (await BarcodeScanner.scan()).rawContent;
       setState(() => this.barcode = barcode);
     } on PlatformException catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
+      if (e.code == BarcodeScanner.cameraAccessDenied) {
         setState(() {
           this.barcode = 'The user did not grant the camera permission!';
         });
