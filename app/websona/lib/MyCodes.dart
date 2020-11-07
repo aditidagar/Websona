86889aa2718b6f6c4835d52b047114480d4ad92a
Saakshi Shah <saakshishah@saakshis-mbp.lan>
2020-11-07 16:23:22 -0500

    Quick fixes

--- app/websona/lib/MyCodes.dart
+++ app/websona/lib/MyCodes.dart
@@ -15,9 +15,16 @@ class _MyCodesState extends State<MyCodes> {
       title: 'My Codes',
       home: Scaffold(
           appBar: AppBar(
+            title: const Text(
+              '  My Codes',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+            ),
+            backgroundColor: Colors.transparent,
+            bottomOpacity: 0.0,
+            elevation: 0.0,
             centerTitle: false,
-            title: Text('My Codes', style: TextStyle(color: Colors.black)),
-            backgroundColor: Colors.white,
+            automaticallyImplyLeading: false,
             actions: <Widget>[
               Padding(
                   padding: EdgeInsets.only(right: 20.0),
@@ -49,26 +56,39 @@ class _MyCodesState extends State<MyCodes> {
                           mainAxisSpacing: 25.0),
                       itemBuilder: (BuildContext ctxt, int index) {
                         return new Container(
-                          height: 50.0,
-                          width: 50.0,
-                          color: Colors.transparent,
-                          child: Container(
+                          width: 200,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            color: Colors.pink,
+                            elevation: 10,
+                            child: Container(
                               decoration: BoxDecoration(
                                   image: DecorationImage(
-                                      fit: BoxFit.cover,
                                       image: AssetImage(
-                                          'asset/img/background.png')),
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(25.0))),
-                              child: new Center(
-                                child: new Text(
-                                  "QR Code",
-                                  style: TextStyle(
-                                      fontSize: 22, color: Colors.white),
-                                  textAlign: TextAlign.center,
-                                ),
-                              )
-                              ),
+                                          'asset/img/background.png'))),
+                            ),
+                          ),
+                          // height: 50.0,
+                          // width: 50.0,
+                          // color: Colors.transparent,
+                          // child: Container(
+                          //     decoration: BoxDecoration(
+                          //         image: DecorationImage(
+                          //             fit: BoxFit.cover,
+                          //             image: AssetImage(
+                          //                 'asset/img/background.png')),
+                          //         borderRadius:
+                          //             BorderRadius.all(Radius.circular(25.0))),
+                          //     child: new Center(
+                          //       child: new Text(
+                          //         "QR Code",
+                          //         style: TextStyle(
+                          //             fontSize: 22, color: Colors.white),
+                          //         textAlign: TextAlign.center,
+                          //       ),
+                          //     )),
                         );
                         // Text(litems[index]);
                       }))
