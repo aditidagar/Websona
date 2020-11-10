a9d04a0b0d53c1cc5c0ae79e4f93c9943f4a5542
Lakshya2610 <lakshyagupta26@live.com>
2020-11-10 16:56:24 -0500

    updated tf config to add dns record for the domain name

--- server-config/variables.tf
+++ server-config/variables.tf
@@ -13,3 +13,13 @@ variable "github_pass" {
     type = string
     description = "Github Password"
 }
+
+variable "dns_zone_id" {
+  type = string
+  description = "Zone ID for the Route53 hosted zone for DNS"
+}
+
+variable "cert" {
+  type = string
+  description = "SSL certificate for the API server"
+}
