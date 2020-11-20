da21af47dbe9930c7aef23a0a6d695e15420a840
Saakshi Shah <saakshishah@Saakshis-MacBook-Pro.local>
2020-11-20 16:32:42 -0500

    Basic contacts page set up

---
+++ app/websona/lib/Contacts.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Contacts extends StatefulWidget {
+  @override
+  _ContactsState createState() => _ContactsState();
+}
+
+class _ContactsState extends State<Contacts> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Contacts',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              '  Contacts',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+            ),
+            backgroundColor: Colors.transparent,
+            bottomOpacity: 0.0,
+            elevation: 0.0,
+            centerTitle: false,
+            automaticallyImplyLeading: false,
+          ),
+        ));
+  }
+}
