d0a6cbef0377ae1a36ad334e56424bd164b8a551
IbrahimFazili <ibrahim.fazili@mail.utoronto.ca>
2020-11-28 13:27:39 -0500

    started on making the events, added dismissible feature

--- app/websona/lib/Events.dart
+++ app/websona/lib/Events.dart
@@ -9,6 +9,28 @@ class _EventState extends State<Event> {
   DateTime selectedDate = DateTime.now();
   List<String> eventItems = [];
   List<String> eventDates = [];
+  List<String> eventNames = [
+    'Harsh Jhunjhunwala',
+    'Aditi Dagar',
+    'Ibrahim Fazili',
+    'Saakshi Shah',
+    'Gautam Gireesh'
+  ];
+  List<String> eventEmails = [
+    'harsh.jhunjhunwala@mail.utoronto.ca',
+    'aditi.dagar@mail.utoronto.ca',
+    'ibrahim.fazili@mail.utoronto.ca',
+    'saakshi.shah@mail.utoronto.ca',
+    'gautam.gireesh@mail.utoronto.ca'
+  ];
+
+  List<String> eventPictures = [
+    'https://picsum.photos/250?image=9',
+    'https://picsum.photos/250?image=10',
+    'https://picsum.photos/250?image=11',
+    'https://picsum.photos/250?image=12',
+    'https://picsum.photos/250?image=13'
+  ];
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime picked = await showDatePicker(
@@ -76,6 +98,82 @@ class _EventState extends State<Event> {
         });
   }
 
+  displayEvent(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              leading: InkWell(
+                  child: new Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.black,
+                    size: 22.0,
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  }),
+              title: const Text(
+                'Events',
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+              ),
+              backgroundColor: Colors.blue,
+              bottomOpacity: 0.0,
+              elevation: 0.0,
+              centerTitle: false,
+              automaticallyImplyLeading: false,
+            ),
+            body: new Container(
+              padding: new EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Align(
+                      alignment: Alignment.centerLeft,
+                      child: new Text("Participants",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 30))),
+                  new Expanded(
+                      child: ListView.separated(
+                    padding: EdgeInsets.only(top: 15),
+                    itemCount: eventNames.length,
+                    separatorBuilder: (BuildContext context, int index) =>
+                        Divider(
+                      color: Colors.white,
+                    ),
+                    itemBuilder: (BuildContext ctxt, int index) {
+                      return new ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(eventPictures[
+                              index]), // no matter how big it is, it won't overflow
+                        ),
+                        tileColor: Colors.blue[50],
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                        title: Text(
+                          eventNames[index],
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        subtitle: Text(eventEmails[index]),
+                        onTap: () {
+                          //code to see each event here
+                        },
+                      );
+                    },
+                  ))
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -126,23 +224,59 @@ class _EventState extends State<Event> {
                 color: Colors.white,
               ),
               itemBuilder: (BuildContext ctxt, int index) {
-                return new ListTile(
-                  tileColor: Colors.blue[50],
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.horizontal()),
-                  title: Text(
-                    eventItems[index],
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  subtitle: Text(eventDates[index]),
-                  trailing: Icon(Icons.list),
-                  onTap: () {
-                    //code to see each event here
-                  },
-                );
+                final item = eventItems[index];
+                return new Dismissible(
+                    key: Key(item),
+                    direction: DismissDirection.endToStart,
+                    onDismissed: (direction) {
+                      // if (direction == DismissDirection.endToStart) {
+                      setState(() {
+                        eventDates.removeAt(index);
+                        eventItems.removeAt(index);
+                      });
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text("Event deleted ")));
+                      // }
+                    },
+                    background: Container(color: Colors.red),
+                    child: ListTile(
+                      tileColor: Colors.blue[50],
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.horizontal()),
+                      title: Text(
+                        eventItems[index],
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600),
+                      ),
+                      subtitle: Text(eventDates[index]),
+                      trailing: InkWell(
+                        child: Icon(Icons.list),
+                        onTap: () {
+                          displayEvent(context);
+                        },
+                      ),
+                    ));
+                // ListTile(
+                //   tileColor: Colors.blue[50],
+                //   shape: RoundedRectangleBorder(
+                //       borderRadius: BorderRadius.horizontal()),
+                //   title: Text(
+                //     eventItems[index],
+                //     style: TextStyle(
+                //         fontSize: 18,
+                //         color: Colors.black,
+                //         fontWeight: FontWeight.w600),
+                //   ),
+                //   subtitle: Text(eventDates[index]),
+                //   trailing: InkWell(
+                //     child: Icon(Icons.list),
+                //     onTap: () {
+                //       displayEvent(context);
+                //     },
+                //   ),
+                // );
               },
             ))
           ],
