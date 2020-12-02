0a5275b3cba0ebb7b6bf02baa31a42a5950785b3
Lakshya Gupta <lakshya.gupta@mail.utoronto.ca>
2020-12-01 23:55:43 -0500

    cleanup. moved code/:id before auth token to allow for scanning codes outside of the app

--- backend/javascript/index.js
+++ backend/javascript/index.js
@@ -66,6 +66,7 @@ app.post("/signup", (req, res) => {
         phone: req.body.phone,
         password: bcrypt_1.default.hashSync(req.body.password, 10),
         activationId: crypto_1.createHash('sha1').update(currentDate + random).digest('hex'),
+        socials: [],
         contacts: []
     };
     DatabaseHandler_1.insertUser(requestData)
@@ -148,6 +149,50 @@ app.post('/updateWebhook', (req, res) => {
     res.status(200);
     res.end();
 });
+app.get("/code/:id", (req, res) => {
+    const codeId = req.params.id;
+    DatabaseHandler_1.fetchCodes({ id: codeId }).then((codes) => __awaiter(void 0, void 0, void 0, function* () {
+        codes = codes;
+        if (codes.length === 0) {
+            res.status(404).send('Code not found');
+            return;
+        }
+        if (!(yield authentication_1.authenticateTokenReturn(req))) {
+            // send the playstore/appstore link page
+            res.status(403).send(`
+            <html>
+                <body>
+                    <span>Please download the app to continue</span>
+                </body>
+            </html>`).end();
+            return;
+        }
+        const email = codes[0].owner;
+        DatabaseHandler_1.fetchUsers({ email }).then((users) => {
+            users = users;
+            if (users.length === 0) {
+                res.status(404).send('User not found');
+                return;
+            }
+            const user = users[0];
+            // delete unneccessary fields
+            delete user.password;
+            delete user.phone;
+            delete user.activationId;
+            delete user.email;
+            delete user.codes;
+            // only provide socials associated with the code
+            user.socials = codes[0].socials;
+            res.status(200).send(user);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send('500: Internal Server Error during db fetch');
+        });
+    })).catch((err) => {
+        console.log(err);
+        res.status(500).send('500: Internal Server Error during db fetch');
+    });
+});
 // routes created after the line below will be reachable only by the clients
 // with a valid access token
 app.use(authentication_1.authenticateToken);
@@ -160,6 +205,45 @@ app.get("/updateProfilePicture", (req, res) => __awaiter(void 0, void 0, void 0,
 app.get("/protectedResource", (req, res) => {
     res.status(200).send("This is a protected resource");
 });
+app.post("/addContact", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+    const user1 = jsonwebtoken_1.default.decode(token).email;
+    const code_id = req.body.code_id;
+    try {
+        const codes = yield DatabaseHandler_1.fetchCodes({ id: code_id });
+        const code = codes[0];
+        DatabaseHandler_1.fetchUsers({ email: user1 }).then((users) => __awaiter(void 0, void 0, void 0, function* () {
+            if (users.length === 0)
+                res.status(404).send("User not found");
+            else {
+                const userContacts = users[0].contacts;
+                const shared = [];
+                for (const x of code.socials) {
+                    shared.push({ social: x.social, username: x.username });
+                }
+                let contactId = null;
+                try {
+                    contactId = (yield DatabaseHandler_1.fetchUsers({ email: code.owner }))[0]._id;
+                }
+                catch (error) {
+                    res.status(500).send("500: Server Error. Failed to add contact").end();
+                }
+                const contact = { id: contactId, sharedSocials: shared };
+                userContacts.push(contact);
+                DatabaseHandler_1.updateUser({ contacts: userContacts }, { email: users[0].email })
+                    .then((val) => res.status(201).send("Contact added successfully"))
+                    .catch((err) => res.status(500).send("500: Server Error. Failed to add contact"));
+            }
+        })).catch((err) => {
+            console.log(err);
+            res.status(500).send('500: Internal Server Error during db fetch');
+        });
+    }
+    catch (error) {
+        return null;
+    }
+}));
 app.get("/user/:email", (req, res) => {
     DatabaseHandler_1.fetchUsers({ email: req.params.email })
         .then((users) => __awaiter(void 0, void 0, void 0, function* () {
@@ -170,7 +254,7 @@ app.get("/user/:email", (req, res) => {
             // generate get urls for all the codes so the app can load the images for the codes
             for (var codes_1 = __asyncValues(codes), codes_1_1; codes_1_1 = yield codes_1.next(), !codes_1_1.done;) {
                 const code = codes_1_1.value;
-                const url = yield AWSPresigner_1.generateSignedGetUrl("codes/" + code.id, 30);
+                const url = yield AWSPresigner_1.generateSignedGetUrl("codes/" + code.id, 120);
                 code.url = url;
             }
         }
@@ -210,55 +294,17 @@ app.post("/updateUser", (req, res) => {
     });
 });
 app.post("/newCode", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a;
-    const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
-    const user1 = jsonwebtoken_1.default.decode(token).email;
-    const code_id = req.body.code_id;
-    try {
-        const codes = yield DatabaseHandler_1.fetchCodes({ id: code_id });
-        const code = codes[0];
-        DatabaseHandler_1.fetchUsers({ email: user1 }).then((users) => __awaiter(void 0, void 0, void 0, function* () {
-            if (users.length === 0)
-                res.status(404).send("User not found");
-            else {
-                const userContacts = users[0].contacts;
-                const shared = [];
-                for (const x of code.socials) {
-                    shared.push({ social: x.social, username: x.username });
-                }
-                let contactId = null;
-                try {
-                    contactId = (yield DatabaseHandler_1.fetchUsers({ email: code.owner }))[0]._id;
-                }
-                catch (error) {
-                    res.status(500).send("500: Server Error. Failed to add contact").end();
-                }
-                const contact = { id: contactId, sharedSocials: shared };
-                userContacts.push(contact);
-                DatabaseHandler_1.updateUser({ contacts: userContacts }, { email: users[0].email })
-                    .then((val) => res.status(201).send("Contact added successfully"))
-                    .catch((err) => res.status(500).send("500: Server Error. Failed to add contact"));
-            }
-        })).catch((err) => {
-            console.log(err);
-            res.status(500).send('500: Internal Server Error during db fetch');
-        });
-    }
-    catch (error) {
-        return null;
-    }
-}));
-app.post("/newCode", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     var _b;
     const codeId = yield getUniqueCodeId();
     if (codeId === null)
         res.status(500).send('500: Internal Server Error during db lookup').end();
     else {
         // generate a PUT URL to allow for qr code upload from client
-        const putUrl = yield AWSPresigner_1.generateSignedPutUrl('codes/' + codeId, 'image/jpeg');
+        const putUrl = yield AWSPresigner_1.generateSignedPutUrl('codes/' + codeId, 'image/png');
         const token = (_b = req.headers.authorization) === null || _b === void 0 ? void 0 : _b.split(' ')[1];
         const decodedToken = jsonwebtoken_1.default.decode(token);
         const socials = req.body.socials;
+        objectCleanup(socials);
         // insert code into db
         DatabaseHandler_1.insertCode({ id: codeId, socials, owner: decodedToken.email }).then((writeResult) => {
             res.status(201).send({ codeId, putUrl });
@@ -272,43 +318,13 @@ app.post("/newCode", (req, res) => __awaiter(void 0, void 0, void 0, function* (
         });
     }
 }));
-app.get("/code/:id", (req, res) => {
-    const codeId = req.params.id;
-    DatabaseHandler_1.fetchCodes({ id: codeId }).then((codes) => {
-        codes = codes;
-        if (codes.length === 0) {
-            res.status(404).send('Code not found');
-            return;
-        }
-        const email = codes[0].owner;
-        DatabaseHandler_1.fetchUsers({ email }).then((users) => {
-            users = users;
-            if (users.length === 0) {
-                res.status(404).send('User not found');
-                return;
-            }
-            const user = users[0];
-            // delete unneccessary fields
-            delete user.password;
-            delete user.phone;
-            delete user.activationId;
-            delete user.email;
-            delete user.codes;
-            // only provide socials associated with the code
-            user.socials = codes[0].socials;
-            res.status(200).send(user);
-        }).catch((err) => {
-            console.log(err);
-            res.status(500).send('500: Internal Server Error during db fetch');
-        });
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send('500: Internal Server Error during db fetch');
-    });
-});
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Listening at http://localhost:${process.env.PORT || PORT}`);
 });
+function getCodeMiddlewareFailCallback(req, res, next) {
+    req.authFailed = true;
+    next();
+}
 /**
  * Generate unique id for a qr code
  */
@@ -339,5 +355,23 @@ function verifyQRupload(codeId) {
             }
         }));
     });
+}
+/**
+ * Delete invalid entries from an object
+ * @param obj object to clean up
+ */
+function objectCleanup(obj) {
+    const keys = Object.keys(obj);
+    const keysToRemove = [];
+    for (const key of keys) {
+        if (key.trim().length === 0)
+            keysToRemove.push(key);
+        else if (key === "null" || key === "undefined")
+            keysToRemove.push(key);
+    }
+    while (keysToRemove.length > 0) {
+        delete obj[keysToRemove[0]];
+        keysToRemove.shift();
+    }
 }
 exports.default = app;
--- backend/typescript/index.ts
+++ backend/typescript/index.ts
@@ -6,13 +6,13 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { createHash } from 'crypto';
 import { insertUser, fetchUsers, updateUser, fetchCodes, insertCode, deleteCode } from "./utils/DatabaseHandler";
-import { authenticateToken, generateAccessToken, tokenExpiryTime } from './authentication';
+import { authenticateToken, authenticateTokenReturn, generateAccessToken, tokenExpiryTime } from './authentication';
 import { SignUpInfo, LoginInfo, User, Code, AccessToken, Contact, PartialUserData } from './interfaces';
 import { MongoError, ObjectId } from 'mongodb';
 import { json as _bodyParser } from 'body-parser';
 import { verifyGithubPayload } from './webhook';
 import { sendVerificationEmail } from './emailer';
-import { generateSignedPutUrl, generateSignedGetUrl} from './AWSPresigner'
+import { generateSignedPutUrl, generateSignedGetUrl } from './AWSPresigner'
 import { stringify } from 'querystring';
 
 const PORT = process.env.PORT;
@@ -57,7 +57,7 @@ app.post("/signup", (req, res) => {
         activationId: createHash('sha1').update(currentDate + random).digest('hex'),
         socials: [],
         contacts: []
-	};
+    };
 
     insertUser(requestData)
         .then(async (result) => {
@@ -143,13 +143,62 @@ app.post('/updateWebhook', (req, res) => {
     res.end();
 });
 
+app.get("/code/:id", (req, res) => {
+    const codeId = req.params.id;
+    fetchCodes({ id: codeId }).then(async (codes) => {
+        codes = codes as Code[];
+        if (codes.length === 0) {
+            res.status(404).send('Code not found');
+            return;
+        }
+
+        if (!(await authenticateTokenReturn(req))) {
+            // send the playstore/appstore link page
+            res.status(403).send(`
+            <html>
+                <body>
+                    <span>Please download the app to continue</span>
+                </body>
+            </html>`).end();
+            return;
+        }
+
+        const email = codes[0].owner;
+        fetchUsers({ email }).then((users) => {
+            users = users as User[];
+            if (users.length === 0) {
+                res.status(404).send('User not found');
+                return;
+            }
+            const user = users[0] as PartialUserData;
+            // delete unneccessary fields
+            delete user.password;
+            delete user.phone;
+            delete user.activationId;
+            delete user.email;
+            delete user.codes;
+            // only provide socials associated with the code
+            user.socials = (codes[0] as Code).socials;
+            res.status(200).send(user);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send('500: Internal Server Error during db fetch');
+        });
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send('500: Internal Server Error during db fetch');
+    })
+});
+
 
 // routes created after the line below will be reachable only by the clients
 // with a valid access token
 app.use(authenticateToken);
 
 app.get("/updateProfilePicture", async (req, res) => {
-    const email = req.query.email;
+    const token = req.headers.authorization?.split(' ')[1] as string;
+    const decodedToken = jwt.decode(token) as AccessToken;
+    const email = decodedToken.email;
     const profilePicture = bcrypt.hashSync(email, 1);
     const url = await generateSignedPutUrl("profile-pictures/" + profilePicture, req.query.type);
     res.status(200).send(url);
@@ -173,8 +222,8 @@ app.post("/addContact", async (req, res) => {
             else {
                 const userContacts = users[0].contacts;
                 const shared = [] as any;
-                for(const x of code.socials){
-                    shared.push({social: x.social, username: x.username})
+                for (const x of code.socials) {
+                    shared.push({ social: x.social, username: x.username })
                 }
                 let contactId: ObjectId | null = null;
                 try {
@@ -182,11 +231,11 @@ app.post("/addContact", async (req, res) => {
                 } catch (error) {
                     res.status(500).send("500: Server Error. Failed to add contact").end();
                 }
-                const contact: Contact = {id: contactId as ObjectId, sharedSocials: shared}
+                const contact: Contact = { id: contactId as ObjectId, sharedSocials: shared }
                 userContacts.push(contact)
                 updateUser({ contacts: userContacts }, { email: users[0].email })
-                .then((val) => res.status(201).send("Contact added successfully"))
-                .catch((err) => res.status(500).send("500: Server Error. Failed to add contact"));
+                    .then((val) => res.status(201).send("Contact added successfully"))
+                    .catch((err) => res.status(500).send("500: Server Error. Failed to add contact"));
             }
 
         }).catch((err) => {
@@ -200,35 +249,38 @@ app.post("/addContact", async (req, res) => {
     }
 
 });
+
 app.get("/user/:email", (req, res) => {
     fetchUsers({ email: req.params.email })
-    .then(async (users: User[] | MongoError) => {
-        const user: PartialUserData = users[0];
-        const codes = await fetchCodes({ owner: user.email }) as Code[];
+        .then(async (users: User[] | MongoError) => {
+            const user: PartialUserData = users[0];
+            const codes = await fetchCodes({ owner: user.email }) as Code[];
 
-        // generate get urls for all the codes so the app can load the images for the codes
-        for await (const code of codes) {
-            const url = await generateSignedGetUrl("codes/" + code.id, 120);
-            code.url = url;
-        }
+            // generate get urls for all the codes so the app can load the images for the codes
+            for await (const code of codes) {
+                const url = await generateSignedGetUrl("codes/" + code.id, 120);
+                code.url = url;
+            }
 
-        user.codes = codes;
-        delete user.password;
+            user.codes = codes;
+            delete user.password;
 
-        res.status(200).send(user);
-    })
-    .catch((err) => {
-        console.log(err);
-        res.status(500).send("Server error");
-    });
+            res.status(200).send(user);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send("Server error");
+        });
 })
 
 app.post("/updateUser", (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1] as string;
+    const decodedToken = jwt.decode(token) as AccessToken;
     const singleUser: PartialUserData = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         phone: req.body.phone,
-        email: req.body.email,
+        email: decodedToken.email,
         socials: req.body.socials
     };
     fetchUsers({ email: singleUser.email }).
@@ -253,6 +305,7 @@ app.post("/newCode", async (req, res) => {
         const token = req.headers.authorization?.split(' ')[1] as string;
         const decodedToken = jwt.decode(token) as AccessToken;
         const socials = req.body.socials;
+        objectCleanup(socials);
         // insert code into db
         insertCode({ id: codeId, socials, owner: decodedToken.email }).then((writeResult) => {
             res.status(201).send({ codeId, putUrl });
@@ -265,42 +318,6 @@ app.post("/newCode", async (req, res) => {
             res.status(500).send('500: Internal Server Error during db insertion');
         });
     }
-});
-
-app.get("/code/:id", (req, res) => {
-    const codeId = req.params.id;
-    fetchCodes({ id: codeId }).then((codes) => {
-        codes = codes as Code[];
-        if (codes.length === 0) {
-            res.status(404).send('Code not found');
-            return;
-        }
-
-        const email = codes[0].owner;
-        fetchUsers({ email }).then((users) => {
-            users = users as User[];
-            if (users.length === 0) {
-                res.status(404).send('User not found');
-                return;
-            }
-            const user = users[0] as PartialUserData;
-            // delete unneccessary fields
-            delete user.password;
-            delete user.phone;
-            delete user.activationId;
-            delete user.email;
-            delete user.codes;
-            // only provide socials associated with the code
-            user.socials = (codes[0] as Code).socials;
-            res.status(200).send(user);
-        }).catch((err) => {
-            console.log(err);
-            res.status(500).send('500: Internal Server Error during db fetch');
-        })
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send('500: Internal Server Error during db fetch');
-    })
 });
 
 app.listen(process.env.PORT || PORT, () => {
@@ -325,6 +342,10 @@ async function getUniqueCodeId() {
     }
 }
 
+/**
+ * Check if the code's image was uploaded to s3. If not, delete entry from the database
+ * @param codeId code id for code whose upload needs to be validated
+ */
 async function verifyQRupload(codeId: string): Promise<void> {
     const downloadUrl = await generateSignedGetUrl('codes/' + codeId, 3000);
     https.get(downloadUrl as string, ((res) => {
@@ -333,6 +354,24 @@ async function verifyQRupload(codeId: string): Promise<void> {
             deleteCode(codeId);
         }
     }));
+}
+
+/**
+ * Delete invalid entries from an object
+ * @param obj object to clean up
+ */
+function objectCleanup(obj: object) {
+    const keys = Object.keys(obj);
+    const keysToRemove: any[] = [];
+    for (const key of keys) {
+        if (key.trim().length === 0) keysToRemove.push(key);
+        else if (key === "null" || key === "undefined") keysToRemove.push(key);
+    }
+
+    while (keysToRemove.length > 0) {
+        delete obj[keysToRemove[0]];
+        keysToRemove.shift();
+    }
 }
 
 export default app;
